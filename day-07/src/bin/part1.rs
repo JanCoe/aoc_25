5e7de8e91d602c355bff37c2040a9ccf2b354b53
JanCoe <jan@jancoe.io>
2025-12-07 16:55:45 +0000

    refactor day 7 part 1

--- day-07/src/bin/part1.rs
+++ day-07/src/bin/part1.rs
@@ -1,40 +1,36 @@
 fn part1(data: &str) -> usize {
-    let mut splitters: Vec<_> = Vec::new();
-    let line_length = data.lines().next().unwrap().trim().len();
-    let beam_start = (line_length - 1) / 2;
+    let mut lines = data.lines();
+    let first_line = lines.next().unwrap().trim();
+    let beam_start = (first_line.len() - 1) / 2;
 
-    // Each row represents positions of splitters
-    splitters.push(vec![beam_start]);
-    data.lines()
-        .skip(1)
-        .map(|line| {
+    let splitter_rows: Vec<Vec<_>> = std::iter::once(vec![beam_start])
+        .chain(lines.map(|line| {
             line.chars()
                 .enumerate()
-                .filter_map(|(ctr, i)| if let '^' = i { Some(ctr) } else { None })
-                .collect::<Vec<_>>()
-        })
-        .for_each(|indices| splitters.push(indices));
+                .filter_map(|(ctr, ch)| (ch == '^').then_some(ctr))
+                .collect()
+        }))
+        .collect();
 
-    let mut old_beams = vec![beam_start];
+    let mut current_beams = vec![beam_start];
     let mut num_splits = 0;
 
-    for row in splitters.into_iter().skip(1) {
-        let mut new_beams = Vec::new();
+    for splitters in splitter_rows.iter().skip(1) {
+        let mut next_beams = Vec::new();
 
-        for &beam in &old_beams {
-            if row.contains(&beam) {
+        for &beam in &current_beams {
+            if splitters.contains(&beam) {
                 num_splits += 1;
-                new_beams.push(beam - 1);
-                new_beams.push(beam + 1);
+                next_beams.push(beam - 1);
+                next_beams.push(beam + 1);
             } else {
-                new_beams.push(beam);
+                next_beams.push(beam);
             }
         }
 
-        new_beams.sort_unstable();
-        new_beams.dedup();
-
-        old_beams = new_beams;
+        next_beams.sort_unstable();
+        next_beams.dedup();
+        current_beams = next_beams;
     }
     num_splits
 }
